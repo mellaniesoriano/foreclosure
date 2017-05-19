9059315625ef598d1e84d549f1f0ebf155435766
mellaniesoriano <msoriano808@gmail.com>
2017-05-18 20:17:52 -1000

    declared variables and created loan func

---
+++ foreclosure.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var steve;
+var stevesLoan;
+var month = 0;
+var monthsUntilEvicted;
+
+function loan(){
+  var account = {
+    borrowed: 550000,
+    balance: 286000,
+    monthlyPayment: 1700,
+    defaulted: 0,
+    defaultsToForeclosure: 5,
+    forclosed: false
+  };
+
+}
